33a8dd3badafc6e6297527d4656b210903bb35e8
Lama El-Sherbiny <lamasherb@gmail.com>
2021-06-29 11:46:08 -0400

    more

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,25 @@ const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET home page. */
-router.get('/', asyncHandler(async (req, res, next) =>  {
-  res.render('login');
+router.get('/', csrfProtection, asyncHandler(async (req, res, next) =>  {
+  res.render('login', {
+      title: 'login',
+      csrfToken: req.csrfToken()
+  });
+
 }));
+const loginValidators = [
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+// router.post('/', csrfProtection, asyncHandler(async (req,res,next) => {}
+
+
+
 
 module.exports = router;
