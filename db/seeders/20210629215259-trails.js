5ea8fea0cd1dab5da2655e418ca9cd52577881d9
Lama El-Sherbiny <lamasherb@gmail.com>
2021-06-29 17:55:37 -0400

    edited

--- db/seeders/20210629215259-trails.js
+++ db/seeders/20210629215259-trails.js
@@ -2,25 +2,20 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
+    
+      return queryInterface.bulkInsert('Trails', [{
         name: 'John Doe',
         isBetaMember: false
       }], {});
-    */
+    
+
+ 
+  
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('Trails', null, {});
+    
   }
 };
