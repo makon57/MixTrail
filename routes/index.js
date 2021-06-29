609ad07c210729ff9e29b624698add33670a7141
Lama El-Sherbiny <lamasherb@gmail.com>
2021-06-29 19:26:54 -0400

    AHHHHHH

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { logoutUser } = require('../auth');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', { title: 'MixTrail' })
 });
-
 router.post('/', (req, res) => {
   logoutUser(req, res);
   res.redirect('/login');
 });
-
 module.exports = router;
