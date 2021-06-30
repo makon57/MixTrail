6f351e38ae99df02ec353f72f299b19313426685
simonvargas <simonvargas01@gmail.com>
2021-06-30 00:39:41 -0400

    added images

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
 // view engine setup
 app.set('view engine', 'pug');
 
+app.use(express.static('images'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
