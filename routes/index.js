72deb4165f286a95164f5489fb310ecc2c7c95e6
Lama El-Sherbiny <lamasherb@gmail.com>
2021-06-30 12:29:47 -0400

    pushed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { logoutUser } = require('../auth');
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MixTrail' })
@@ -9,4 +14,25 @@ router.post('/', (req, res) => {
   logoutUser(req, res);
   res.redirect('/login');
 });
+
+
+router.get('/', asyncHandler(async (req, res) => {
+  const trails = await db.Trails.findAll()
+  
+
+  
+
+}))
+
+
+
+
+
+  res.render('home.pug', {
+    listTitle: 'Latest Recipes',
+    error,
+    recipes,
+  });
+
+
 module.exports = router;
