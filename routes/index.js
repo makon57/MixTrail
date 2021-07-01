01f11de483dfb51bdb97117485da5b8e48ee1efb
Manna Kong <m.kong.57@gmail.com>
2021-06-30 19:39:54 -0500

    done withh login signup on HP

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,29 @@ const { logoutUser } = require('../auth');
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 
+const getUserFromSession = async req => {
+  if (req.session.auth) {
+      const id = parseInt(req.session.auth.userId);
+      let user = await db.User.findByPk(id);
+      if (user) {
+          user = user.toJSON();
+          return {userId: user.id, username: user.username};
+      } else {
+          req.session.save(err => {
+              if (err) return next(err);
+              delete req.session.auth;
+          })
+      }
+  }
+  return false;
+}
 
 router.get('/', csrfProtection, asyncHandler(async(req, res) => {
   const trails = await db.Trail.findAll();
-  res.render('index', ({ trails }));
+  const user = await getUserFromSession(req)
+  res.render('index', ({ trails, user }));
 }));
+
 
 router.get('/trail/:id(\\d+)', csrfProtection, asyncHandler(async(req, res, next) => {
   const trailId = parseInt(req.params.id, 10);
