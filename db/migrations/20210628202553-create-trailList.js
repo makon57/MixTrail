1134ecffad20822e37c5863226052b86151a5ffd
simonvargas <simonvargas01@gmail.com>
2021-07-01 22:58:10 -0400

    gotta fix delete

---
+++ db/migrations/20210628202553-create-trailList.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TrailLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        unique: true,
+        allowNull: false,
+       type: Sequelize.STRING(255)
+      },
+      userId: {
+      references: { model: "Users" },
+      allowNull: false,
+       type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('TrailLists');
+  }
+};
